db0e6c102d427d1d87078f21f43451ceeea4953e
praveen choudhary <pc852180@gmail.com>
2024-09-16 18:29:40 +0530

    frist program in js

---
+++ day1 js/class.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.log("This is our frist javascript program");
+// output
+// variable in js
+let num1 = "praveen choudhary";
+let num2 = 45;
+let char = 10==10;
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(char));
---
+++ day1 js/class1.js
@@ -0,0 +1,13 @@
+// console.log("hello");
+// console.log("This is our frist program");
+
+
+let num1 = 30;
+let num2 = 50;
+let num3 = "praveen"
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(num3));
+
+// cout 
