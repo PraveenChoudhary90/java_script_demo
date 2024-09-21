44077d5efe3d44fe5d8c0de17bbf10a15a882057
praveen choudhary <pc852180@gmail.com>
2024-09-21 17:41:14 +0530

    document object model

---
+++ 6day js/dom.js
@@ -0,0 +1,9 @@
+function dom(){
+    let a = document.querySelector(".heading");//.textContent;
+    a.style.color = "red";
+    a.style.fontSize = "30px";
+    let b = document.querySelector("#text");//.textContent;
+    b.style.color = "blue";
+    console.log(a);
+    console.log(b);
+}
---
+++ 6day js/style.js
@@ -0,0 +1,8 @@
+function doc(){
+    let a = document.querySelector(".heading");
+    a.style.color = "red";
+    let b = document.querySelector("#text");
+    b.style.color = "green";
+    console.log(a);
+    console.log(b);
+}
