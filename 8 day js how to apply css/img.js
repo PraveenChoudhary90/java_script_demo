81325630951f78f6205cb9deccd9e1eb53763e1e
praveen choudhary <pc852180@gmail.com>
2024-09-24 15:54:43 +0530

    how to apply css

---
+++ 8 day js how to apply css/img.js
@@ -0,0 +1,4 @@
+function hello(){
+    let selecteddiv = document.querySelector("#img2");
+    selecteddiv.style.backgroundImage = "url('https://media.istockphoto.com/id/1534040386/photo/an-outdoor-bamboo-gazebo-nestled-amidst-lush-greenery-offering-serene-relaxation-after-a.jpg?s=1024x1024&w=is&k=20&c=puHgSbTQpBTWvqIBd69gDXvBgDwpor6-fdYJoU0Hihc=')";
+}
---
+++ 8 day js how to apply css/index.js
@@ -0,0 +1,8 @@
+function change(){
+    let selecteddiv = document.querySelector("#img1");
+    selecteddiv.style.backgroundImage = "url('/body4.jpg')";
+    selecteddiv.style.backgroundRepeat = "no-repeat";
+    selecteddiv.style.backgroundSize = "cover";
+    selecteddiv.style.border = "2px solid red";
+
+}
