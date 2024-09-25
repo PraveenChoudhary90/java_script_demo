c50364611e2f2d324a38704085098c1bdd5331f2
praveen choudhary <pc852180@gmail.com>
2024-09-25 18:21:24 +0530

    click product to see big image

---
+++ 9 day js apply css/product.js
@@ -0,0 +1,16 @@
+function change1(){
+    let selectimg = document.querySelector("#lagreimg");
+    selectimg.src = "/water-8100724_960_720.jpg";
+}
+function change2(){
+    let selectimg = document.querySelector("#lagreimg");
+    selectimg.src= "/tree-736885_960_720.jpg";
+}
+function change3(){
+    let selectimg = document.querySelector("#lagreimg");
+    selectimg.src= "/lake-192990_960_720.jpg"  ;
+}
+function change4(){
+    let selectimg = document.querySelector("#lagreimg");
+    selectimg.src= "/hut-8843868_960_720.jpg" ;
+}
