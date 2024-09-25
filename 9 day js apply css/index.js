c79a350219f7a269cf478b03808ee3dce587865b
praveen choudhary <pc852180@gmail.com>
2024-09-25 17:57:56 +0530

    how to apply css onmouseenter using event

---
+++ 9 day js apply css/index.js
@@ -0,0 +1,6 @@
+function run(){
+    let selectdul = document.querySelector('ul');
+    selectdul.type = "square";
+    let selectphoto = document.querySelector('#photo');
+    selectphoto.src = "/lake-192990_960_720.jpg"
+}
---
+++ 9 day js apply css/photo.js
@@ -0,0 +1,19 @@
+function change1(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src="/water-8100724_960_720.jpg";
+}
+
+function change2(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "/tree-736885_960_720.jpg";
+}
+
+function change3(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "/lake-192990_960_720.jpg";
+}
+
+function change4(){
+    let selectedlargeimg = document.querySelector('#largeimg');
+    selectedlargeimg.src = "/italy-mountains-dawn-daybreak-147411.jpeg" ;
+}
