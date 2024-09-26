23834b7f0365e8afb8b75432e493d730de3774de
praveen choudhary <pc852180@gmail.com>
2024-09-26 15:49:49 +0530

    ragistraction from

---
+++ 10 day js user input/index.js
@@ -0,0 +1,17 @@
+function user(){
+    let selectedname = document.querySelector("#name").value;
+    let selectedage = document.querySelector("#age").value;
+    let selectedemail = document.querySelector("#email").value;
+    let selectedpassword = document.querySelector("#pass").value;
+    let selectedconformPassword = document.querySelector("#confrom").value;
+    let selectedresetPassword = document.querySelector("#reset").value;
+    console.log(selectedname);
+    console.log(selectedage);
+    console.log(selectedemail);
+    console.log(selectedpassword);
+    console.log(selectedconformPassword);
+    console.log(selectedresetPassword);
+
+    return false;
+
+}
