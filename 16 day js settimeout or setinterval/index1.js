6928fc6e9999b8c132cdb533a0b7bf8336abfd54
praveen choudhary <pc852180@gmail.com>
2024-10-04 15:23:07 +0530

    setTimeout  higher order function

---
+++ 16 day js settimeout or setinterval/index1.js
@@ -0,0 +1,22 @@
+function data()
+{
+    setTimeout(show , 10000);
+        function show(){
+        let site = document.querySelector("#website");
+        site.style.display = "block";
+        let image = document.querySelector("#img");
+        image.style.display = "none";
+        let heading = document.querySelector("#heading");
+        heading.style.display = "none";
+        let button = document.querySelector("#btn");
+        button.style.display = "none";
+          let heading1 = document.querySelector("#heading1");
+        heading1.style.display = "none";
+        let button1 = document.querySelector("#btn1");
+        button1.style.display = "none";
+
+
+
+        }
+    
+}
