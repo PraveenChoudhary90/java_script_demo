1ed64c4779e41dbdaac2b04b4bed46c313dc6e48
praveen choudhary <pc852180@gmail.com>
2024-10-14 15:14:19 +0530

    array index lenght and in or of for loop

---
+++ 22 day js Array/arr.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,8,9,0 ,"Praveen"];
+console.log(arr);
+console.log(arr[5]);
+console.log(arr[10][5]);
+console.log(typeof(arr));
+for(let i in arr){
+    console.log(i);
+}
+for(let i of arr){
+    console.log(i);
+}
