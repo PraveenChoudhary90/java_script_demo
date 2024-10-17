3f15ed8e227e36de1fc63504428dd59e1f655815
praveen choudhary <pc852180@gmail.com>
2024-10-17 11:56:35 +0530

    map higher method

---
+++ 25 day js array  higher mthods/index.js
@@ -0,0 +1,6 @@
+let arr = [12,234,22,55,123,3,4,5,6,7,8];
+let newarr = arr.map((items) => {return items*2});
+console.log(newarr);
+
+let arr1 = arr.map((news)=>{return news+10});
+console.log(arr1);
