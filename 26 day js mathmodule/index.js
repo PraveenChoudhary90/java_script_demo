8a0f5b05fc6e39046cbee3cec53128d29d7a4d1d
praveen choudhary <pc852180@gmail.com>
2024-10-18 14:15:12 +0530

    mathmodule in js

---
+++ 26 day js mathmodule/index.js
@@ -0,0 +1,29 @@
+let r = "hello";
+r.charAt(3);
+console.log(r);
+r.charCodeAt(3);
+console.log(r);
+
+let a = Math.PI;
+console.log(a);
+
+let b = Math.sqrt();
+console.log(b);
+
+let c  = Math.sqrt(1/2);
+console.log(c);
+
+
+let d = Math.pow(3,4);
+console.log(d);
+
+let e = Math.round(4.5);
+console.log(e);
+
+
+ let str = ["1,2,3,4,5,6,7,8,66,9,444,33,22"];
+ let len = str.length;
+let ran = Math.random();
+let ans = len * ran;
+let output = str.charAt(ans);
+console.log(ans);
