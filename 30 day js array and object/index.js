dc95b585f155bbdcbb92b6693b07b79041651802
praveen choudhary <pc852180@gmail.com>
2024-10-23 11:58:16 +0530

    variable of array and object

---
+++ 30 day js array and object/index.js
@@ -0,0 +1,25 @@
+let a = {
+    "id":1,
+    "name":"praveen",
+    "age":22,
+    "course":"mern",
+    "number":9098354728
+}
+let {name,id,age,course,number} = a;
+console.log(id);
+console.log(name);
+console.log(age);
+console.log(course);
+console.log(number);
+//method
+let b = {
+    "sum":function()
+    {
+
+    }
+}
+b.sum()
+//function
+function show(){
+
+}
