80b8b5f7bdf590d1748d40a7f44076c69e55b716
praveen choudhary <pc852180@gmail.com>
2024-11-09 14:59:36 +0530

    operator and conditional statements

---
+++ 34 day operater in java script/ifcondition.js
@@ -0,0 +1,8 @@
+let age = window.prompt("Enter the age");
+if(age > 18)
+{
+    window.alert("you can vote");
+}
+else{
+    window.alert("you can not vote")
+}
---
+++ 34 day operater in java script/script1.js
@@ -0,0 +1,10 @@
+// airthmetic opratore
+let a = 20;
+let b = 30;
+console.log("a=",a,"b=",b)
+console.log( "a+b = ",a+b);
+console.log( "a-b = ",a-b);
+console.log( "a*b = ",a*b);
+console.log( "a/b = ",a/b);
+console.log( "a%b = ",a%b);
+
