ff675ad6583d0bea85ac965319afd0b7dd56a46d
praveen choudhary <pc852180@gmail.com>
2024-11-15 14:56:47 +0530

    localstorage in frontend

--- 36day in js/object1.js
+++ 36day in js/object1.js
@@ -11,3 +11,20 @@ let obj = {
 
 localStorage.setItem("data",JSON.stringify(obj));
 }
+ let m = JSON.parse(localStorage.getItem("data"));
+console.log(m);
+
+
+
+
+let myname = document.querySelector("#myname");
+let myage = document.querySelector("#myage");
+let mycontact = document.querySelector("#mycontact");
+let myemail = document.querySelector("#myemail");
+
+
+myname.innerHTML = m.name,
+myage.innerHTML = m.age,
+mycontact.innerHTML = m.contact,
+myemail.innerHTML = m.email
+
