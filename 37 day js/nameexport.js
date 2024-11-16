d261f82ed377e3e9db07aab493c9f5c7cce17f65
praveen choudhary <pc852180@gmail.com>
2024-11-16 15:07:21 +0530

    module import and export

---
+++ 37 day js/nameexport.js
@@ -0,0 +1,10 @@
+function a()
+{
+    console.log("a function");
+}
+
+function b()
+{
+    console.log("b function");
+}
+export {a,b};
---
+++ 37 day js/script1.js
@@ -0,0 +1,8 @@
+import sum from './add.js';
+
+import {a,b} from './nameexport.js';
+
+console.log(sum(10,20));
+
+a();
+b();
