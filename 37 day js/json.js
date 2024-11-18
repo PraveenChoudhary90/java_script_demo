e957018b169fbc253a530e9718a7a6ba57513437
praveen choudhary <pc852180@gmail.com>
2024-11-18 12:43:46 +0530

    json fack api

---
+++ 37 day js/json.js
@@ -0,0 +1,15 @@
+ async  function fet()
+{
+    let res =  await fetch("https://jsonplaceholder.typicode.com/comments");
+    let a = await res.json();
+
+    // console.log(a);
+     let answ = a.map((e)=>`
+        <h1>${e.id}</h1>
+        <h1>${e.email}</h1>
+    `).join("");
+
+    document.querySelector('#demo').innerHTML = answ
+
+}
+fet();
