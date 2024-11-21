765d9629d105fbac71385e05da984d47bcb69b4c
praveen choudhary <pc852180@gmail.com>
2024-11-21 23:41:11 +0530

    crud in js delete

---
+++ CRUDE IN JAVA SCRIPT/delete.js
@@ -0,0 +1,27 @@
+async function fet()
+{
+    let res = await fetch("http://localhost:3000/students");
+    let data = await res.json();
+    console.log(data);
+    let result = data.map((e)=>`
+    
+      <tr>
+        <th>${e.id}</th>
+        <th>${e.name}</th>
+        <th>${e.age}</th>
+        <th>${e.address}</th>
+        <td><button onclick ="deletedata('${e.id}');">delete</button></td>
+    </tr>
+
+    `).join(" ");
+    document.querySelector("#showdata").innerHTML = result;
+}
+fet();
+ function deletedata(id){
+
+let deletefinal = fetch(`http://localhost:3000/students/${id}`,{
+    method:'DELETE'
+})
+.then(re=>alert("data is sucessfully deleted"));
+
+};
