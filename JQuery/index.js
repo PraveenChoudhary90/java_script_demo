5313a8c063822217280de19e19065c48782bc02a
praveen choudhary <pc852180@gmail.com>
2024-11-23 12:30:21 +0530

    jQuery

---
+++ JQuery/index.js
@@ -0,0 +1,36 @@
+$(document).ready(
+    function(){
+
+        $('h1').click(function(){
+              window.alert("ready....!!!!!")
+
+        })
+
+
+         $('h2').mouseenter(function(){
+            window.alert("praveen")
+        })
+         $('h2').mouseleave(function(){
+            window.alert("praveen..leave")
+        })
+
+        //  $('h1').onload(function(){
+        //     window.alert("praveen")
+        // })
+
+    
+
+
+})
+// $(document).ready(function()
+// {
+//     $('h1').click(function(){
+//     alert("hello")
+// })
+// })
+//syntax of jquery
+
+//$() is selector
+// .ready function work when html page is load sucessfully
+
+//function();=>
