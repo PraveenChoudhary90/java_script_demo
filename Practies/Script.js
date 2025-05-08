cc7868db8b51ae790a0d4a0ba043e4e2e36c5fe1
praveen choudhary <pc852180@gmail.com>
2025-05-08 16:28:35 +0530

    Java Script

---
+++ Practies/Script.js
@@ -0,0 +1,18 @@
+
+console.log("Hello this is praveen Kumar CHoudhary");
+
+function sum (){
+    let a = 90;
+    let b = 90;
+    console.log(a+b);
+}
+
+sum();
+
+
+function hello(){
+    var a = parseInt(window.prompt("Enter the frist number"));
+    let b =parseInt(window.prompt("Enter the sencond number"));
+    window.alert(a+b); 
+}
+hello();
