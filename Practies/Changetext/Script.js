382aa07f833df644e4112a9d723b0aba2dffaaa5
praveen choudhary <pc852180@gmail.com>
2025-05-09 13:48:22 +0530

    Confirm

---
+++ Practies/Changetext/Script.js
@@ -0,0 +1,11 @@
+
+
+function call() {
+    const a = document.querySelector("#data");
+    a.style.color = "red";
+    a.style.fontSize = "30px"
+
+    const b = document.querySelector("#head");
+    b.style.color = "blue";
+    b.style.fontSize = "30px";
+}
