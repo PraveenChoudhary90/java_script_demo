315dfede4e96e7015fc2e1a295692ffc8870d1c3
praveen choudhary <pc852180@gmail.com>
2025-05-12 14:48:58 +0530

    Imgae Zoom in

---
+++ Practies/ImageZoomIn/Script.js
@@ -0,0 +1,12 @@
+function zoomin(){
+    let a = document.querySelector("#img");
+    a.style.scale = "1.2";
+    a.style.transition = "scale 1.5s";
+}
+
+function simple(){
+    let b = document.querySelector("#img")
+    b.style.scale= "1";
+    b.style.transition = "scale 1.5s";
+    
+}
